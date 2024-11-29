b06242ba1ec36a76669773183f4b8970fb231120
NataliaKengil <241283nata@gmail.com>
2024-11-29 23:29:20 +0300

    init: start strings tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,8 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value1, value2) {
+  return value1 + value2;
 }
 
 /**
